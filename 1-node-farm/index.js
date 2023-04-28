a53b519d3d4ab21cda396075f1c1cf16809c6092
Dhruv Arora <dhruvarora.code@gmail.com>
2023-04-28 22:43:27 +0530

    fs added

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,8 @@
 const hello = "Hello world";
 console.log(hello);
 
-console.log('hello world created');
+//Node.js is really built around this concept of modules where all kind of additional functionality is stored in a module and in the case of reading files that is inside the FS module:
+const fs = require("fs"); //fs --> file system
+//so by using this module here we will get access to functions for reading data and writing data right to the file system so again calling this function here with this built-in fs module name will then return an object in which there are lots of functions that we can use and we storr that object right into the fs variable that we can then later use
+
+
