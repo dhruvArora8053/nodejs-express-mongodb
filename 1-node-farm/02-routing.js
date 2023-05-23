2b02159df9be9705b99874f34603cda602b229d5
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-23 23:15:33 +0530

    implemented routing

--- 1-node-farm/02-routing.js
+++ 1-node-farm/02-routing.js
@@ -5,8 +5,25 @@ const server = http.createServer((req, res) => {
   console.log(req.url);
 
   //implementing routing
+  const pathName = req.url;
 
-  res.end("Hello from the server!");
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  }
+
+  //fallback
+  else {
+    res.writeHead(404, {
+      //sending headers
+      //an http header is basically a piece of information about the response that we are sending back:
+      "Content-type": "text/html", //now browser is expection some html
+      "my-own-header": "hello-world",
+    });
+    //now what matter here is that these headers and also the status code always need to be set before we send out the response so we can never send header after the response content itself
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 // this callback function gets called for 2 times, first time for the url and the second time for favicon(browser automatically sends off the request for favicon)
 
