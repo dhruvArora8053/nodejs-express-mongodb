1a964e0b226640775e77bf5c93b89991fde321a7
Dhruv Arora <dhruvarora.code@gmail.com>
2023-05-25 20:09:26 +0530

    implemented templating

--- 1-node-farm/04-htmlTemplating.js
+++ 1-node-farm/04-htmlTemplating.js
@@ -2,7 +2,23 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
-//from here
+const replaceTemplate = (template, product) => {
+  let output = template.replaceAll("{%PRODUCTNAME%}", product.productName);
+  output = output.replaceAll("{IMAGE}", product.image);
+  output = output.replaceAll("{PRICE}", product.price);
+  output = output.replaceAll("{FROM}", product.from);
+  output = output.replaceAll("{NUTRIENTS}", product.nutrients);
+  output = output.replaceAll("{QUANTITY}", product.quantity);
+  output = output.replaceAll("{DESCRIPTION}", product.description);
+  output = output.replaceAll("{ID}", product.id);
+  
+
+  if (!product.organic)
+    output = output.replaceAll("{NOT_ORGANIC}", "not-organic");
+
+  return output;
+};
+
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
@@ -16,6 +32,10 @@ const tempProduct = fs.readFileSync(
   "utf-8"
 );
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+//again dataObj is an array at this point of time with five objects in it, so now what we have to do is to basically loop through this array and for each of them replace the placeholders in the template with the actual data from the current product: inside of server
+
 const server = http.createServer((req, res) => {
   console.log(req.url);
 
@@ -24,7 +44,15 @@ const server = http.createServer((req, res) => {
   //Overview Page
   if (pathName === "/" || pathName === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
-    res.end(tempOverview);
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    // console.log(cardsHtml);
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+
+    // res.end(tempOverview);
+    res.end(output);
 
     //Product Page
   } else if (pathName === "/product") {
