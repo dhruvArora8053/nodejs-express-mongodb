39a0b57a24b175ba697c978c04a08dd20a814e2b
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-05 15:55:33 +0530

    implemented promise

--- 3-asynchronous-JS/02-promises.js
+++ 3-asynchronous-JS/02-promises.js
@@ -3,16 +3,18 @@ const superagent = require("superagent");
 
 //Promises
 fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-    console.log(`Breed: ${data}`);
-  
-    superagent
-      .get(`https://dog.ceo/api/breed/${data}/images/random`)
-      .then((res) => {
-        if (err) return console.log(err.message);
-        console.log(res.body.message);
-  
-        fs.writeFile("dog-img.txt", res.body.message, (err) => {
-          console.log("random dog image saved to file");
-        });
+  console.log(`Breed: ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`)
+    .then((res) => {
+      console.log(res.body.message);
+
+      fs.writeFile("dog-img.txt", res.body.message, (err) => {
+        console.log("random dog image saved to file");
       });
-  });
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
