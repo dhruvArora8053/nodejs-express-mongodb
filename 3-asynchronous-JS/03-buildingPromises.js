b879ac1aef4ed18ee31e8f4564c061c85f3ebd1e
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-05 16:31:51 +0530

    built promise

---
+++ 3-asynchronous-JS/03-buildingPromises.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const superagent = require("superagent");
+
+const readFilePromise = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I could not find that file");
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePromise = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("Could not write file");
+      resolve("success");
+    });
+  });
+};
+
+readFilePromise(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+    return writeFilePromise("dog-img.txt", res.body.message);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
