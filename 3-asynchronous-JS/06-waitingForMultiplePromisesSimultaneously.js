37714ae455fe5269688fac2d6f395cd266a7f676
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-05 17:54:27 +0530

    implemented multiple awaits

---
+++ 3-asynchronous-JS/06-waitingForMultiplePromisesSimultaneously.js
@@ -0,0 +1,65 @@
+const fs = require("fs");
+const superagent = require("superagent");
+
+const readFilePromise = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject("I could not find that file");
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePromise = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject("Could not write file");
+      resolve("success");
+    });
+  });
+};
+
+const getDogPic = async () => {
+  try {
+    const data = await readFilePromise(`${__dirname}/dog.txt`);
+    console.log(`Breed: ${data}`);
+
+    const res1Promise = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const res2Promise = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const res3Promise = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const all = await Promise.all([res1Promise, res2Promise, res3Promise]);
+    const imgs= all.map(el=>el.body.message)
+    console.log(imgs);
+
+    // console.log(res.body.message);
+
+    await writeFilePromise("dog-img.txt", imgs.join('\n'));
+    console.log("Random dog image saved to file");
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+
+  return "2: READY";
+};
+
+(async () => {
+  try {
+    console.log("1: Will get dog pics!");
+    const x = await getDogPic();
+    console.log(x);
+    console.log("3: Done getting dog pics!");
+  } catch (err) {
+    console.log("ERROR!");
+  }
+})();
+
