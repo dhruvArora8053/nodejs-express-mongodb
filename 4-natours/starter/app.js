ccd132bf90b5a0c31beaa199604171e7c2581b53
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-05 18:44:36 +0530

    set up express

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,9 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res)=>{
-    
-}) //.get is an http method
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
+}); //.get is an http method
+ 
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoint...');
+});
 
 const port = 3000;
 app.listen(port, () => {
