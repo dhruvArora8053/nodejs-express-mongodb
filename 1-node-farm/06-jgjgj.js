1411951ebccda3df61b7314eaec00e2033bda89b
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-06 18:37:25 +0530

    file added

--- 1-node-farm/06-jgjgj.js
+++ 1-node-farm/06-jgjgj.js
@@ -1,11 +1,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-
 const slugify = require("slugify");
-
 const replaceTemplate = require("./modules/replaceTemplate");
-// "." here represents where the current module is
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -26,51 +23,22 @@ const dataObj = JSON.parse(data);
 const slugs = dataObj.map((el) => slugify(el.productName), { lower: true });
 console.log(slugs);
 
-console.log(slugify("Fresh Avocados", { lower: true }));
-
 const server = http.createServer((req, res) => {
-  //from here
-  //   console.log(req.url);
-  //   console.log(url.parse(req.url, true));
-
   const { query, pathname } = url.parse(req.url, true);
-  //here parse has divided the url into pieces so whenever the req will have /product then it will directly sends the response whatever the pathaname===/product going to have
-  //   const pathName = req.url;
 
-  //Overview page
   if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
       .join("");
-    // console.log(cardsHtml);
 
     const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
     res.end(output);
-
-    //Product page
   } else if (pathname === "/product") {
     res.writeHead(200, { "Content-type": "text/html" });
-    // console.log(query);
     const product = dataObj[query.id];
     const output = replaceTemplate(tempProduct, product);
-
     res.end(output);
-
-    //API
-  } else if (pathname === "/api") {
-    res.writeHead(200, { "Content-type": "application/json" });
-    res.end(data);
-  }
-
-  //Not found
-  else {
-    res.writeHead(404, {
-      "Content-type": "text/html",
-      "my-own-header": "hello-world",
-    });
-
-    res.end("<h1>Page not found!</h1>");
   }
 });
 
