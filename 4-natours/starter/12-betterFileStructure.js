dd0a604d69879197af1d8a8448b5956a225ed293
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-07 15:36:40 +0530

    implementing file structure

---
+++ 4-natours/starter/12-betterFileStructure.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter= require('./routes/tourRoutes')
+const userRouter= require('./routes/userRoutes')
+
+const app = express();
+
+//1.)MIDDLEWARES
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware!');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+//4.)START SERVER
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
