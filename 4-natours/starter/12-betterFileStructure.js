86e3ac38d74c447a5c65aaa9e7de92366ce6f995
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-07 19:11:04 +0530

    served static files

--- 4-natours/starter/12-betterFileStructure.js
+++ 4-natours/starter/12-betterFileStructure.js
@@ -10,8 +10,10 @@ const app = express();
 
 //1.)MIDDLEWARES
 app.use(morgan('dev'));
-
 app.use(express.json());
+
+//serving static files
+app.use(express.static(`${__dirname}/public`))
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware!');
