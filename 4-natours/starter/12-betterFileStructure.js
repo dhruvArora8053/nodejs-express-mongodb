a6a91f5570b2fb37fd0f351f16176b1548960a6a
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-07 20:51:26 +0530

    implemented environment variables

--- 4-natours/starter/12-betterFileStructure.js
+++ 4-natours/starter/12-betterFileStructure.js
@@ -4,16 +4,19 @@ const morgan = require('morgan');
 //FROM HERE:-
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-//so we have our routers now each in one different file and we can say that each of them is one small sub-application so i.e. one user application and one tour application and then we put everything in our global app file by importing above routers and then mouting the routers on the two different routes 
+//so we have our routers now each in one different file and we can say that each of them is one small sub-application so i.e. one user application and one tour application and then we put everything in our global app file by importing above routers and then mouting the routers on the two different routes
 
 const app = express();
 
 //1.)MIDDLEWARES
+console.log(process.env.NODE_ENV);
+
 app.use(morgan('dev'));
+
 app.use(express.json());
 
 //serving static files
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware!');
@@ -30,5 +33,4 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 //4.)START SERVER
-module.exports= app;
-
+module.exports = app;
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,18 @@
-const app= require('./12-betterFileStructure')
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+//this command will read our variables from the file and save them into nodejs environment variables
 
-const port = 3000;
+const app = require('./12-betterFileStructure');
+
+
+
+//Environment Variables:
+console.log(app.get('env'));
+//environment variables are global variables that are used to define the environment in which a node app is running, now this 'env' variable is actually set by express but node.js itself actually also set a lot of environment variables:
+// console.log(process.env);
+//node uses these environment varialbes internally
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
