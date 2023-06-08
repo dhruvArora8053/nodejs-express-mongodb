a22234e2433dfeee4e96942095fd9c06774a4b57
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-08 19:50:14 +0530

    created tour model

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -17,9 +17,26 @@ mongoose
     useFindAndModify: false,
   })
   .then((con) => {
-    console.log(con.connections);
-    console.log('DB connection successful!');
+    // console.log(con.connections);
+    // console.log('DB connection successful!');
   });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
 
 //Environment Variables:
 console.log(app.get('env'));
