e8fb095d8aa75253da0c7029c9e420587ef685fd
Dhruv Arora <dhruvarora.code@gmail.com>
2023-06-08 19:29:19 +0530

    connection implemented

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,8 +1,25 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./12-betterFileStructure');
 
 dotenv.config({ path: './config.env' });
 //this command will read our variables from the file and save them into nodejs environment variables
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful!');
+  });
 
 //Environment Variables:
 console.log(app.get('env'));
@@ -16,4 +33,3 @@ app.listen(port, () => {
 });
 
 const x = 23;
-
